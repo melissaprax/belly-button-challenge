df4c0372834e115770a977881228122c7f73c584
melprax <melissaprax@gmail.com>
2023-08-02 11:19:05 -0500

    displaying default plots and iterating through data

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,36 @@
 // The url with data
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+// Display the default plots
+function init() {
+
+    // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+
+    // Fetch the JSON data and console log it
+    d3.json(url).then((data) => {
+        console.log(`Data: ${data}`);
+
+        // An array of id names
+        let names = data.names;
+
+        // Iterate through the names Array
+        names.forEach((name) => {
+            // Append each name as an option to the drop down menu
+            // This is adding each name to the html file as an option element with value = a name in the names array
+            dropdownMenu.append("option").text(name).property("value", name);
+        });
+
+        // Assign the first name to name variable
+        let name = names[0];
+
+        // Call the functions to make the demographic panel, bar chart, and bubble chart
+        demo(name);
+        bar(name);
+        bubble(name);
+        gauge(name);
+    });
+}
 
 
 // Make the bar chart
@@ -115,4 +145,6 @@ function gauge(selectedValue) {
          Plotly.newPlot("gauge", trace);
     });
 }
+
+
 
